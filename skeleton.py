3838f0d19287982c7dabc8490bb167b8aba9bdb4
Dome <166693723+DomeGitHub101@users.noreply.github.com>
2025-01-09 02:56:39 +0700

    Update skeleton.py

--- skeleton.py
+++ skeleton.py
@@ -2,282 +2,214 @@ class User:
     def __init__(self, citizen_id: str, name: str):
         self.__citizen_id = citizen_id
         self.__name = name
-        self.__acc_list = []
+        self.__accounts = []
 
-    def get_name(self):
+    @property
+    def citizen_id(self):
+        return self.__citizen_id
+
+    @property
+    def name(self):
         return self.__name
 
     @property
-    def acc_list(self):
-        return self.__acc_list
+    def accounts(self):
+        return self.__accounts
 
-    def add_account(self, acc_num):
-        self.__acc_list.append(acc_num)
-    
-    @property
-    def account(self):
-        return self.__acc_list
-    
+    def add_account(self, account):
+        if isinstance(account, Account):
+            self.__accounts.append(account)
+
+
 class Account:
-    def __init__(self, account_number: str, owner: User):
+    def __init__(self, account_number: str, owner: User, initial_balance: float):
         self.__account_number = account_number
         self.__owner = owner
-        self.__current_money = 0
-        self.__atm_card = None
-        self.__reports = []
-    
-    def show_reports(self):
-        print("hello 1")
-        for report in self.get_reports():
-            print("hello")
-            print(report)
-            return self.get_reports()
-        
-    def get_reports(self):
-        return self.__reports
-    
-    def increase_current_money(self,amount):
-        self.__current_money = self.__current_money+amount
-    
-    def decrease_current_money(self,amount):
-        self.__current_money = self.__current_money-amount
-    
-    def set_money(self,money):
-        self.__current_money = money
-        
-    def set_card(self,card_number):
-        self.__atm_card = card_number
-        
-    def get_card(self):
-        return self.__atm_card
+        self.__balance = initial_balance
 
-    def get_account_number(self):
+
+    @property
+    def account_number(self):
         return self.__account_number
 
-    def get_username(self):
+    @property
+    def owner(self):
         return self.__owner
 
-    def get_current_money(self):
-        return self.__current_money
-
-    def change_money(self,action,amount = 0):
-        if(amount <= 0):
-            print("Wrong amount")
-            return "Error monay naja"
-        else:
-            if action == "+":
-                self.__current_money = self.__current_money + amount
-                return self.__current_money
-            elif action == "-":
-                if(self.__current_money>amount):
-                    self.__current_money = self.__current_money - amount
-                else:
-                    return "Error"
-
-    def report(self,save): ##save transaction
-        self.__reports.append(save.report_out())
-        print(save.report_out())
+    @property
+    def balance(self):
+        return self.__balance
     
+    @balance.setter
+    def balance(self, value: float):
+        self.__balance = value
+    
+    
+
+
 class ATMCard:
     def __init__(self, card_number: str, account: Account, pin: str):
         self.__card_number = card_number
         self.__account = account
         self.__pin = pin
-        
-    def get_pin(self):
-        return self.__pin
-        
-    def get_card_number(self):
+
+    @property
+    def card_number(self):
         return self.__card_number
 
+    @property
+    def account(self):
+        return self.__account
+
+    @property
+    def pin(self):
+        return self.__pin
+
+
 class ATMMachine:
-    def __init__(self, machine_id: str, initial_amount: float = 1000000):
+    def __init__(self, machine_id: str, initial_amount: float):
         self.__machine_id = machine_id
-        self.__initial_amount = initial_amount
-        # pass# Class Code
-        
-    def get_machine_id(self):
+        self.__cash_available = initial_amount
+
+    @property
+    def machine_id(self):
         return self.__machine_id
-        
-    def input_atm_card(self,bank,card_num,pin):
-        for user in bank.get_user_list():
-            for acc in user.account:
-                if acc.get_card().get_card_number() == card_num:
-                    if pin != acc.get_card().get_pin():
-                        return "Invalid PIN"
+    @property
+    def cash_available(self):
+        return self.__cash_available
+
+    def input_atm_card(self, bank: "Bank", card_number: str, entered_pin: str):
+        # ค้นหาบัตร ATM
+        for user in bank.user_list:
+            for account in user.accounts:
+                card = account.card
+                if card and card.card_number == card_number:
+                    if card.pin == entered_pin:
+                        print(f"{card.card_number}, {card.account.account_number}, Success")
+                        return
                     else:
-                        print(f"{acc.get_card().get_card_number()} , {acc.get_account_number()} , Success")
-                        return acc
-                else:
-                    print("acc dont exist")
-        return None
-    
-    def deposit(self,bank,acc,amount):
-        if amount < 1:
-            return "Error"
+                        print("Invalid PIN")
+                        return
+        print("Card Not Found")
+
+    def deposit(self,bank : "Bank", acc : str,amount : float):
+        if amount < 0:
+            return print("Error")
+
         else:
-            for user in bank.get_user_list():
-                for account in user.account:
-                    if account.get_account_number() == acc:
-                        print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                        account.change_money("+",amount)
-                        print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                        self.__initial_amount = self.__initial_amount + amount
-                        
-                        account.report(Transaction(account.get_username().get_name(),"D",self.__machine_id,account.get_current_money()-amount,account.get_current_money()))
+            for user in bank.user_list:
+                for account in user.accounts:
+                    if account.account_number == acc:
+                        print(f"{user.name} account before deposit: {account.balance}")
+                        account.balance += amount
+                        print(f"{user.name} account after deposit: {account.balance}")
                         return "Success"
-        return "Error"
-    
-    def Withdraw(self,bank,acc,amount):
-        if amount < 1 or amount > self.__initial_amount:
-            return "Error"
-        elif amount > 40000:
-            print("Exceeds daily withdrawal limit of 40,000 baht")
-            return "Error"
-        else: 
-            for user in bank.get_user_list():
-                for account in user.account:
-                    if amount > account.get_current_money():
-                        print("Error Not enough money")
-                        return "Error"
-                    elif account.get_account_number() == acc:
-                        print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                        account.change_money("-",amount)
-                        print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                        self.__initial_amount = self.__initial_amount + amount
-                        # def __init__(self,username,status : str,atm_machine,money_before,money_aft
-                        account.report(Transaction(account.get_username().get_name(),"W",self.__machine_id,account.get_current_money()+amount,account.get_current_money()))
-                        return "Success"
-        return "Error"
-    
-    def Transfer(self,bank,atm_num,acc_self,acc_other,amount,):
-        # Status1 = acc_self.change_money("-",amount)
-        # Status2 = acc_other.change_money("+",amount)
-        # acc_self.report("TW",amount,atm_num,acc_other)
-        # acc_other.report("TD",amount,atm_num,acc_self) 
+            return print("Error: Account not found")
+
+    def withdraw(self,bank : "Bank", acc : str,amount : float):
+        for user in bank.user_list:
+                for account in user.accounts:
+                    if account.account_number == acc:
+                        if amount > account.balance:
+                            return print("Error")
+                        else:
+                            print(f"{user.name} account before deposit: {account.balance}")
+                            account.balance -= amount
+                            print(f"{user.name} account after deposit: {account.balance}")
+                            return "Success"
+        return print("Error: Account not found")
         
-        # if Status1 != "Error" and Status2 != "Error":
-        #     return "Success"
-        # else:
-        #     return "Error"
-        if amount < 1 or amount > self.__initial_amount  or amount > 40000:
-            return "Error"
-        # else: 
-            # for user in bank.get_user_list():
-            #     for account in user.account:
-            #         if amount > account.get_current_money():
-            #             print("Error Not enough money")
-            #             return "Error"
-            #         elif account.get_account_number() == acc_self:
-            #             count+=1
-            #             if count == 2:
-                            
-                            # Status1 = acc_self.change_money("-",amount)
-                            # Status2 = acc_other.change_money("+",amount)
-                            
-                            
-                            
-                            # print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                            # account.change_money("-",amount)
-                            # print(f"{account.get_username().get_name()}account before test : {account.get_current_money()}")
-                            # self.__initial_amount = self.__initial_amount + amount
-                            # account.report(Transaction(account.get_username().get_name(),"W",self.__machine_id,account.get_current_money()+amount,account.get_current_money()))
-                            # if Status1 != "Error" and Status2 != "Error":
-                            #     return "Success"    
-        return "Error"
-   
-class Bank:  
+
+
+class Bank:
     def __init__(self):
         self.__user_list = []
         self.__atm_list = []
-    
-    #'1-1101-12345-31-0':['Harry Potter','1234567890','12345',20000]
-    def register(self,user_info : dict):
-        for i in user_info.keys():
-            user = User(i,user_info[i][0])
-            acc = Account(user_info[i][1],user)
-            card = ATMCard(user_info[i][2],acc,"1234")
-            
-            user.add_account(acc)
-            self.__user_list.append(user)
-            acc.set_money(user_info[i][3])
-            acc.set_card(card)
 
-    def atm_data(self,atm_info : dict):
-        for j in atm_info.keys():
-            self.__atm_list.append(ATMMachine(j,atm_info[j]))
-    
+    def register(self, user_info: dict):
+        for citizen_id, data in user_info.items():
+            name, account_number, card_number, initial_balance = data
+            user = User(citizen_id, name)
+            account = Account(account_number, user, initial_balance)
+            card = ATMCard(card_number, account, "1234")
+            account.card = card
+            user.add_account(account)
+            self.__user_list.append(user)
+
+    def atm_data(self, atm_info: dict):
+        for machine_id, initial_cash in atm_info.items():
+            self.__atm_list.append(ATMMachine(machine_id, initial_cash))
+
     @property
     def user_list(self):
         return self.__user_list
-    
+
     @property
     def atm_list(self):
         return self.__atm_list
-    
-    def get_user_list(self):
-        return self.__user_list
-    
-class Transaction():
-    def __init__(self,username,status : str,atm_machine,money_before,money_after):
-        self.__username = username
-        self.__status = status
-        self.__atm_machine = atm_machine
-        self.__money_before = money_before
-        self.__money_after = money_after
-        self.__report = f"{self.__username} transaction : {self.__status}-{self.__atm_machine}-{self.__money_before}-{self.__money_after}"
-    #Hermione transaction : D-ATM:1002-1000-2000
-    def report_out(self):
-        return self.__report
-################################################################################################################################################
-# กำหนดรูปแบบของ user ดังนี้ {
-    #        รหัสประชาชน :       [ชื่อ,       หมายเลขบัญชี, หมายเลข ATM_card ,จำนวนเงิน ]}
-a_user ={   '1-1101-12345-31-0':['Harry Potter','1234567890','12345',20000],
-            '1-1101-12345-32-0':['Hermione Jean Granger','0987654321','12346',1000]}
 
-a_atm ={'Bangna':1000000,'Ladkrabang':200000}
-aBank = Bank()
-aBank.register(a_user)
-aBank.atm_data(a_atm)
+    
 
-# Test case #1 : ทดสอบ การ insert บัตร โดยค้นหาเครื่อง atm เครื่องที่ 1 และบัตร atm ของ harry    //
+##################################################################################
+
+# กำหนดให้ ชื่อคลาส (Class Name) ต้องเป็น Pascal case เช่น BankAccount
+# กำหนดให้ ชื่อ instance และ variables ใดๆ ต้องเป็น snake case เช่น my_book
+# กำหนดให้ เมื่อรับพารามิเตอร์เข้าใน method ต้องทำ validate data type และกรอบของค่า parameter ก่อนใช้เสมอ
+# กำหนดให้ method ที่จัดการข้อมูลใด ต้องอยู่ในคลาสนั้น และพยายามอย่า access attribute นอกคลาส
+
+# กำหนดรูปแบบของ user ดังนี้ {รหัสประชาชน : [ชื่อ, หมายเลขบัญชี, หมายเลข ATM , จำนวนเงิน]}
+user_data = {'1-1101-12345-12-0':['Harry Potter','1234567890','12345',20000],
+       '1-1101-12345-13-0':['Hermione Jean Granger','0987654321','12346',1000]}
+   
+atm_dict ={'1001':1000000,'1002':200000}
+
+bank = Bank()
+bank.register(user_data)
+bank.atm_data(atm_dict)
+
+
+
+# Test case #1 : ทดสอบ การ insert บัตร โดยค้นหาเครื่อง atm เครื่องที่ 1 และบัตร atm ของ harry
 # และเรียกใช้ function หรือ method จากเครื่อง ATM
 # ผลที่คาดหวัง : พิมพ์ หมายเลข account ของ harry อย่างถูกต้อง และ พิมพ์หมายเลขบัตร ATM อย่างถูกต้อง
+print("Test case #1\n")
+bank.atm_list[0].input_atm_card(bank, '12345', '1234')
+print("\n-----------------------\n")
 # Ans : 12345, 1234567890, Success
-# input_atm_card(self,bank,card_num,pin)
-aBank.atm_list[0].input_atm_card(aBank,'12345',"1234")
-print("-------------------------")
 
-# Test case #2 : ทดสอบฝากเงินเข้าในบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 1000 บาท //
+
+# Test case #2 : ทดสอบฝากเงินเข้าในบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 1000 บาท
 # ให้เรียกใช้ method ที่ทำการฝากเงิน
 # ผลที่คาดหวัง : แสดงจำนวนเงินในบัญชีของ Hermione ก่อนฝาก หลังฝาก และ แสดง transaction
+print("Test case #2\n")
+bank.atm_list[1].deposit(bank,"0987654321",1000)
+print("\n-----------------------\n")
 # Hermione account before test : 1000
 # Hermione account after test : 2000
-#deposit(self,atm_num,acc,amount):
-aBank.atm_list[1].deposit(aBank,"0987654321",1000)
-print("-------------------------")
 
 
-# Test case #3 : ทดสอบฝากเงินเข้าในบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน -1 บาท //
+# Test case #3 : ทดสอบฝากเงินเข้าในบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน -1 บาท
+print("Test case #3\n")
+bank.atm_list[1].deposit(bank,"0987654321",-1)
+print("\n-----------------------\n")
 # ผลที่คาดหวัง : แสดง Error
-# def deposit(self,atm_num,acc,amount):
-# aBank.atm_list[0].deposit(1234567890,1234567890,-1)
-print(aBank.atm_list[1].deposit(aBank,"0987654321",-1))
-print("-------------------------")
 
-# Test case #4 : ทดสอบการถอนเงินจากบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 500 บาท //
+
+# Test case #4 : ทดสอบการถอนเงินจากบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 500 บาท
 # ให้เรียกใช้ method ที่ทำการถอนเงิน
 # ผลที่คาดหวัง : แสดงจำนวนเงินในบัญชีของ Hermione ก่อนถอน หลังถอน และ แสดง transaction
+print("Test case #4\n")
+bank.atm_list[1].withdraw(bank,"0987654321",500)
+print("\n-----------------------\n")
 # Hermione account before test : 2000
 # Hermione account after test : 1500
-aBank.atm_list[1].Withdraw(aBank,"0987654321",500)
-print("-------------------------")
 
 
-# Test case #5 : ทดสอบถอนเงินจากบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 2000 บาท //
+# Test case #5 : ทดสอบถอนเงินจากบัญชีของ Hermione ในเครื่อง atm เครื่องที่ 2 เป็นจำนวน 2000 บาท
+print("Test case #5\n")
+bank.atm_list[1].withdraw(bank,"0987654321",2000)
+print("\n-----------------------\n")
 # ผลที่คาดหวัง : แสดง Error
-aBank.atm_list[1].Withdraw(aBank,"0987654321",2000)
-print("-------------------------")
 
 # Test case #6 : ทดสอบการโอนเงินจากบัญชีของ Harry ไปยัง Hermione จำนวน 10000 บาท ในเครื่อง atm เครื่องที่ 2
 # ให้เรียกใช้ method ที่ทำการโอนเงิน
@@ -293,25 +225,18 @@ print("-------------------------")
 # Hermione transaction : D-ATM:1002-1000-2000
 # Hermione transaction : W-ATM:1002-500-1500
 # Hermione transaction : TD-ATM:1002-10000-11500
-print(aBank.user_list[0].acc_list[0].show_reports())
-print("-------------------------")
 
-# Test case #8 : ทดสอบการใส่ PIN ไม่ถูกต้อง     //
+# Test case #8 : ทดสอบการใส่ PIN ไม่ถูกต้อง 
 # ให้เรียกใช้ method ที่ทำการ insert card และตรวจสอบ PIN
 # atm_machine = bank.get_atm('1001')
 # test_result = atm_machine.insert_card('12345', '9999')  # ใส่ PIN ผิด
 # ผลที่คาดหวัง
 # Invalid PIN
-print(aBank.atm_list[0].input_atm_card(aBank,'12345',"9999"))
-print("-------------------------")
 
-# Test case #9 : ทดสอบการถอนเงินเกินวงเงินต่อวัน (40,000 บาท)    //
+# Test case #9 : ทดสอบการถอนเงินเกินวงเงินต่อวัน (40,000 บาท)
 # atm_machine = bank.get_atm('1001')
 # account = atm_machine.insert_card('12345', '1234')  # PIN ถูกต้อง
 # harry_balance_before = account.get_balance()
-aBank.atm_list[1].Withdraw(aBank,"0987654321",45000)
-print("-------------------------")
-
 
 # print(f"Harry account before test: {harry_balance_before}")
 # print("Attempting to withdraw 45,000 baht...")
@@ -334,15 +259,18 @@ print("-------------------------")
 # print(f"ATM machine balance after: {atm_machine.get_balance()}")
 # print("-------------------------")
 
-# TODO 1 : จากข้อมูลใน user ให้สร้าง instance โดยมีข้อมูล //
+
+
+# TODO 1 : จากข้อมูลใน user ให้สร้าง instance ของผู้ใช้ โดยมีข้อมูล
 # TODO :   key:value โดย key เป็นรหัสบัตรประชาชน และ value เป็นข้อมูลของคนนั้น ประกอบด้วย
 # TODO :   [ชื่อ, หมายเลขบัญชี, หมายเลขบัตร ATM, จำนวนเงินในบัญชี]
 # TODO :   return เป็น instance ของธนาคาร
 # TODO :   และสร้าง instance ของเครื่อง ATM จำนวน 2 เครื่อง
+# TODO :   ต้อง validate ข้อมุลทุกอย่าง ก่อนสร้าง instance ใดๆ
 
 
-# TODO 2 : เขียน method ที่ทำหน้าที่สอดบัตรเข้าเครื่อง ATM มี parameter 2 ตัว ได้แก่ 1) instance ของธนาคาร //
-# TODO     2) atm_card เป็นหมายเลขของ atm_card
+# TODO 2 : เขียน method ที่ทำหน้าที่สอดบัตรเข้าเครื่อง ATM มี parameter 2 ตัว ได้แก่ 1) instance ของธนาคาร
+# TODO     2) instance ของ atm_card
 # TODO     return ถ้าบัตรถูกต้องจะได้ instance ของ account คืนมา ถ้าไม่ถูกต้องได้เป็น None
 # TODO     ควรเป็น method ของเครื่อง ATM
 
@@ -366,4 +294,3 @@ print("-------------------------")
 # TODO     การทำงาน ให้ลดจำนวนเงินในบัญชีตนเอง และ เพิ่มเงินในบัญชีคนที่โอนไป และ สร้าง transaction ลงในบัญชี
 # TODO     return หากการทำรายการเรียบร้อยให้ return success ถ้าไม่เรียบร้อยให้ return error
 # TODO     ต้อง validate การทำงาน เช่น ตัวเลขต้องมากกว่า 0 และ ไม่ถอนมากกว่าเงินที่มี
-
